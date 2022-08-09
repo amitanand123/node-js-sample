1968c03647a5223f8e677f515f9115715d42a65d
amit anand <amitanand123@users.noreply.github.com>
2022-08-09 14:05:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Testing DevOps Node! JS Project Hello How are you ? welcome to devOps Project')
+  response.send('Hello Testing DevOps Node! JS Project Hello How are you ? welcome to devOps Project with new data')
 }) 
 
 app.listen(app.get('port'), function() {
